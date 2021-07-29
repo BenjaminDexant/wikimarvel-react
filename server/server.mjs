bfe8dc1cc8dc4220988063fbf351e639286ee4f5
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2021-07-29 11:54:26 +0200

    create server using express & graphql

---
+++ server/server.mjs
@@ -0,0 +1,12 @@
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+
+import schema from "./schema.mjs";
+
+const port = 5000;
+
+const app = express();
+
+app.use(graphqlHTTP({ schema, graphiql: true }));
+
+app.listen(port, console.log(`Running on port ${port}`));
