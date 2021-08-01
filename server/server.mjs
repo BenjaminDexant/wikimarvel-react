25108e6ffe46cde293cf5348198031eac7708386
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2021-08-02 00:14:18 +0200

    add graphql folder && install cors middleware

--- server/server.mjs
+++ server/server.mjs
@@ -1,13 +1,14 @@
 import {} from "dotenv/config.js";
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
+import cors from "cors";
 
-import schema from "./schema.mjs";
+import schema from "./graphqlSchema/schema.mjs";
 
 const port = 5000;
 
 const app = express();
 
-app.use(graphqlHTTP({ schema, graphiql: true }));
+app.use(cors(), graphqlHTTP({ schema, graphiql: true }));
 
 app.listen(port, console.log(`Running on port ${port}`));
