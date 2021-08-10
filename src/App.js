dad2ebd174a50782d233993b2e758712215b2077
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2021-08-10 23:46:28 +0200

    copyright Marvel API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Events from "./components/events/Events";
+import Footer from "./components/footer/footer";
 
 const App = () => {
 	return (
 		<div>
 			<Events />
+			<Footer />
 		</div>
 	);
 };
