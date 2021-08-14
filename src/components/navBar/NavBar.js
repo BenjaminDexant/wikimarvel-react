4a485cf3f469303a131ddf4e7609f2b535029271
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2021-08-14 15:03:31 +0200

    css navbar

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -5,22 +5,22 @@ import "./navBar.css";
 const NavBar = () => {
 	return (
 		<ul className="list">
-			<li>
+			<li className="element">
 				<Link activeClass="active" to="home" spy={true} smooth={true}>
 					Home
 				</Link>
 			</li>
-			<li>
+			<li className="element">
 				<Link to="searchCharacter" spy={true} smooth={true}>
 					Search
 				</Link>
 			</li>
-			<li>
+			<li className="element">
 				<Link to="events" spy={true} smooth={true}>
 					Events
 				</Link>
 			</li>
-			<li>
+			<li className="element">
 				<Link to="footer" spy={true} smooth={true}>
 					Footer
 				</Link>
