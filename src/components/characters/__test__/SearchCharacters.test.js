833feae7fe4249111804ee4dcd50c89ad70c7620
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2021-08-24 14:07:10 +0200

    update tests for searchchar componenet

--- src/components/characters/__test__/SearchCharacters.test.js
+++ src/components/characters/__test__/SearchCharacters.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import SearchCharacter from "../SearchCharacter";
 import { fireEvent, render } from "@testing-library/react";
 import renderer from "react-test-renderer";
+import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/extend-expect";
 
 test("The form is displayed correctly", () => {
@@ -11,16 +12,15 @@ test("The form is displayed correctly", () => {
 
 test("Change value is working correctly", () => {
 	const { getByTestId } = render(<SearchCharacter />);
-	const inputElem = getByTestId("input");
+	const inputElem = getByTestId("characterName");
 
 	expect(inputElem.value).toBe("");
 
 	fireEvent.change(inputElem, {
 		target: {
-			value: "Spider-Man",
+			value: "test",
 		},
 	});
 
-	expect(inputElem.value).toBe("Spider-Man");
+	expect(inputElem.value).toBe("test");
 });
-
